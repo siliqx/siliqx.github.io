587e2b8adf2771ac54694dee92c5f36a44cf7219
mirusausiliq <mirusausiliq@gmail.com>
2023-02-08 01:39:10 +0800

    Initialize: Setup the website for Entendemos ʕ•ᴥ•ʔ

---
+++ _nuxt/339e9da.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{347:function(n,e,t){"use strict";t.r(e);var l={name:"StyleVue"},c=t(54),component=Object(c.a)(l,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
